8714af16d32cbe3a9e4482ec5c7ff0fc9df5793e
SwarmPathAI <SwarmPathAI@users.noreply.github.com>
2026-07-07 01:04:09 +0800

    v0.6.3: Homebrew formula (new sha) + release notes + npm 0.6.3

--- Formula/swarmcode.rb
+++ Formula/swarmcode.rb
@@ -1,9 +1,9 @@
 class Swarmcode < Formula
   desc "Provider-agnostic AI coding agent for your terminal"
   homepage "https://github.com/SwarmPathAI/swarmcode"
-  version "0.6.1"
-  url "https://github.com/SwarmPathAI/swarmcode/releases/download/v0.6.1/swarmcode-v0.6.1-darwin-universal.tar.gz"
-  sha256 "257cf1a1983a55c3884c2041f99a0a915953eca00bd7db5ce4e1760e8862ceda"
+  version "0.6.3"
+  url "https://github.com/SwarmPathAI/swarmcode/releases/download/v0.6.3/swarmcode-v0.6.3-darwin-universal.tar.gz"
+  sha256 "efdcfa52d222c324202481fb5d804b4cfba3b92c5a2f12838662c32724a45456"
 
   def install
     bin.install "swarmcode"
