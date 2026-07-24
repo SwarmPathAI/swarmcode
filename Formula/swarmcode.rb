856896c3ca1e21fced85f784bb9708eef233ec19
SwarmPathAI <SwarmPathAI@users.noreply.github.com>
2026-07-24 11:50:29 +0800

    v0.12.0: refresh Homebrew formula sha (in-place binary patch)

--- Formula/swarmcode.rb
+++ Formula/swarmcode.rb
@@ -3,7 +3,7 @@ class Swarmcode < Formula
   homepage "https://github.com/SwarmPathAI/swarmcode"
   version "0.12.0"
   url "https://github.com/SwarmPathAI/swarmcode/releases/download/v0.12.0/swarmcode-v0.12.0-darwin-universal.tar.gz"
-  sha256 "e3285665e4d3bb99ce57203f4aa976de8ca2718e55772d7fe87ad46474286104"
+  sha256 "08eafccc5c3a19d03c4a2985ec37f94bd526962a22e0ed68df01522f748ba00a"
 
   def install
     bin.install "swarmcode"
